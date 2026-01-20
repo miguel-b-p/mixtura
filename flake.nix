75b2b10f3b37ecd1d9626f7316283f125c297350
Mingas <miguelpinotty@gmail.com>
2026-01-20 12:44:48 -0300

    chore: Update project version to 1.0.

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       {
         packages.default = pkgs.python3Packages.buildPythonApplication {
           pname = "mixtura";
-          version = "1.3";
+          version = "1.0";
           pyproject = true;
 
           src = ./.;
